31acb89882d978cb6b29d28b68e6094b72e0a34a
Daria Platonova <searching.for.a.sign@yandex.ru>
2021-01-18 17:59:26 +0300

    Updates

---
+++ precache-manifest.075cb47ba947f197b521be1d1df7b7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764d89ffcc2450896bd515d421f8f4fd",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "a39829e67db5b88f6000",
+    "url": "/to-do-app/static/css/main.8be0afbb.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/to-do-app/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39829e67db5b88f6000",
+    "url": "/to-do-app/static/js/main.41db1319.chunk.js"
+  },
+  {
+    "revision": "05ec602100718e7451dc",
+    "url": "/to-do-app/static/js/runtime-main.781402f6.js"
+  }
+]);
